0f04132f9ae76737be79d0379e78ff70f7cf83d2
LyonKz <38329746+LyonKz@users.noreply.github.com>
2019-12-04 10:18:36 +0800

    Updates

---
+++ precache-manifest.ba4e520a2c83f43d131c3a6ce6969780.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4bc792a5e75d96ef079b545fc5375a",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "d05f9f58ab55d7fed754",
+    "url": "/testapp/static/js/2.088fff24.chunk.js"
+  },
+  {
+    "revision": "a59c0bd1fcea282d1e4e",
+    "url": "/testapp/static/js/main.31ded820.chunk.js"
+  },
+  {
+    "revision": "054168d388959695f1cd",
+    "url": "/testapp/static/js/runtime-main.d0450f9d.js"
+  }
+]);
